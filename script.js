f47b7b90e79f843f9643c8ed0f5a209f5c89d422
SakshiBhojwani <97778989+SakshiBhojwani@users.noreply.github.com>
2023-03-02 23:34:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+ let weather={
+    "apikey":"4542001153f7ae5e72e3a831c4c6c08a",
+    fetchWeather:function(city){
+      fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +"&units=metric&appid=4542001153f7ae5e72e3a831c4c6c08a"
+    ).then((response)=>response.json())
+    .then((data)=>this.displayWeather(data));
+      },
+   displayWeather:function(data){
+      const {name}=data;
+      const {icon,description}=data.weather[0];
+      const{temp,humidity}=data.main;
+      const {speed}=data.wind;
+      document.querySelector(".city").innerText="Weather in "+name;
+      document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText=description;
+      document.querySelector(".temp").innerText=temp+"°C";
+      document.querySelector(".humidity").innerText="Humidity:"+humidity+"%";
+      document.querySelector(".wind").innerText="wind speed:"+speed+"km/h";
+      document.querySelector(".weather").classList.remove("loading");
+   } ,
+   search: function(){
+      this.fetchWeather(document.querySelector(".search-bar").value);
+   } ,
+ };
+
+ document.querySelector(".search button").addEventListener("click",function(){
+   weather.search();
+ });
+
+ document.querySelector(".search-bar").addEventListener("keyup",function(event){
+   if(event.key=="Enter"){
+      weather.search();
+   }
+ });
+
+ weather.fetchWeather("Mumbai");
